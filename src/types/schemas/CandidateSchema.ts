4dd4b84f8c0d9555db1225563d17f680954ded07
Pinchas <139820013+pmk248@users.noreply.github.com>
2024-11-11 14:07:40 +0200

    colors made enum

--- src/types/schemas/CandidateSchema.ts
+++ src/types/schemas/CandidateSchema.ts
@@ -19,6 +19,7 @@ const candidateSchema = new Schema<candidateDocument>({
     },
     color: {
         type     : String,
+        enum     : ['red', 'blue', 'yellow'],
         required : true
     }
 }, { timestamps: true })
